a4c91bf6eefe1ca8dbd2ebdc0484b5531643466a
Oscar Lara <oscar.lara.carretero@gmail.com>
2021-03-21 23:43:14 +0100

    Cierre de ejercicio de detalle

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,6 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
@@ -22,6 +23,7 @@ import { MatRadioModule } from '@angular/material/radio';
     MatInputModule,
     MatSelectModule,
     MatRadioModule,
+    MatTableModule
   ],
   exports: [
     MatButtonModule,
@@ -32,6 +34,7 @@ import { MatRadioModule } from '@angular/material/radio';
     MatInputModule,
     MatSelectModule,
     MatRadioModule,
+    MatTableModule
   ]
 })
 export class MaterialModule { }
