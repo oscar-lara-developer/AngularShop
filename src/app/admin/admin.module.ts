4e9bed91dfbfe37bbc0c49f3c43b3be39f79e51c
Oscar Lara <oscar.lara.carretero@gmail.com>
2021-04-02 00:24:57 +0200

    Añadiendo al carrito de forma reactiva

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -23,9 +23,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { FormProductComponent } from './components/form-product/form-product.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent, TableComponent, DashboardComponent, ProductsListComponent, FormProductComponent],
+  declarations: [ProductFormComponent, NavComponent, TableComponent, DashboardComponent, ProductsListComponent, FormProductComponent, ProductEditComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
